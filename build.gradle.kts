48c90078b90f351900602665bd459be406f290fe
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-12-14 16:54:07 +0300

    switch from in-memory h2 to local mysql

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("it.skrape:skrapeit:1.1.5")
+	implementation(dependencyNotation = "mysql:mysql-connector-java:8.0.30")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.h2database:h2")
 	runtimeOnly("mysql:mysql-connector-java:8.0.32")
