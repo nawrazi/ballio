ba336cdee89e9db2750841216ac8ef14bf888de0
Nazrawi Demeke <nazrawi.demeke@gmail.com>
2023-12-27 22:10:02 +0300

    support other leagues

--- src/main/kotlin/nazrawi/ballio/entitiy/League.kt
+++ src/main/kotlin/nazrawi/ballio/entitiy/League.kt
@@ -3,7 +3,11 @@ package nazrawi.ballio.entitiy
 import nazrawi.ballio.exceptions.UnknownLeagueException
 
 enum class League(val id: Int, val sourceUri: String) {
-    PremierLeague(1, "premierleague");
+    PremierLeague(1, "premierleague"),
+    LaLiga(2, "laligafootball"),
+    SerieA(3, "serieafootball"),
+    League1(4, "ligue1football"),
+    Championship(5, "championship");
 
     companion object {
         fun getBySlug(slug: String): League {
