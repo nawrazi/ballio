22965a2268b1135bb1df22fb85dfaaf503f8ce6b
Nazrawi Demeke <nazrawi.demeke@gmail.com>
2023-12-28 14:38:47 +0300

    parameterize tests

--- src/test/kotlin/nazrawi/ballio/LeagueTests.kt
+++ src/test/kotlin/nazrawi/ballio/LeagueTests.kt
@@ -3,30 +3,51 @@ package nazrawi.ballio
 import nazrawi.ballio.entitiy.League
 import nazrawi.ballio.exceptions.UnknownLeagueException
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class LeagueTests {
 
-    @Test
-    fun testDisplayName() {
-        assertEquals("Premier League", League.PremierLeague.displayName)
-        assertEquals("La Liga", League.LaLiga.displayName)
-        assertEquals("Serie A", League.SerieA.displayName)
-        assertEquals("League 1", League.League1.displayName)
-        assertEquals("Championship", League.Championship.displayName)
+    @ParameterizedTest(name = "Display name of {0} should be {1}")
+    @MethodSource("getLeagueDisplayNames")
+    fun testDisplayName(league: League, expectedDisplayName: String) {
+        assertEquals(league.displayName, expectedDisplayName)
     }
 
-    @Test
-    fun testGetBySlug() {
-        assertEquals(League.PremierLeague, League.getBySlug("premier-league"))
-        assertEquals(League.LaLiga, League.getBySlug("la-liga"))
-        assertEquals(League.SerieA, League.getBySlug("serie-a"))
-        assertEquals(League.League1, League.getBySlug("league1"))
-        assertEquals(League.Championship, League.getBySlug("championship"))
+    @ParameterizedTest(name = "League from slug {0} should be {1}")
+    @MethodSource("getLeagueSlugs")
+    fun testGetBySlug(slug: String, expectedLeague: League) {
+        assertEquals(League.getBySlug(slug), expectedLeague)
 
         assertThrows<UnknownLeagueException> {
-            League.getBySlug("unknown-slug")
+            League.getBySlug("$slug-x")
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun getLeagueDisplayNames(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(League.PremierLeague, "Premier League"),
+                Arguments.of(League.LaLiga, "La Liga"),
+                Arguments.of(League.SerieA, "Serie A"),
+                Arguments.of(League.League1, "League 1"),
+                Arguments.of(League.Championship, "Championship"),
+            )
+        }
+
+        @JvmStatic
+        fun getLeagueSlugs(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("premier-league", League.PremierLeague),
+                Arguments.of("la-liga", League.LaLiga),
+                Arguments.of("serie-a", League.SerieA),
+                Arguments.of("league1", League.League1),
+                Arguments.of("championship", League.Championship),
+            )
         }
     }
 }
